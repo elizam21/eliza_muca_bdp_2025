1a299ae34e0982a33c60a4a5f519f16833390ad6
elizam21 <emuca2107@gmail.com>
2025-10-10 22:05:21 +0200

    Update zad5m.sql

--- 01_setup/zad5m.sql
+++ 01_setup/zad5m.sql
@@ -1,1 +1,6 @@
+SELECT 
+    SUM(pe.kwota + COALESCE(pr.kwota, 0)) AS suma_wszystkich_wynagrodzen
+FROM ksiegowosc.wynagrodzenie w
+JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
+LEFT JOIN ksiegowosc.premia pr ON w.id_premii = pr.id_premii;
 
