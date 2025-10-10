6a0cf5ae30b73a04cfe6af955cbc75a3458a3dd8
elizam21 <emuca2107@gmail.com>
2025-10-10 20:27:35 +0200

    Update zad5e.sql

--- 01_setup/zad5e.sql
+++ 01_setup/zad5e.sql
@@ -1,4 +1,5 @@
 SELECT *
 FROM ksiegowosc.pracownicy
-WHERE nazwisko LIKE '%n%'   -- nazwisko zawiera literę 'n'
-  AND imie LIKE '%a';       -- imię kończy się literą 'a'
+WHERE nazwisko LIKE '%n%'   
+  AND imie LIKE '%a';      
+
