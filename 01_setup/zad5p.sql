2cd6185f6fb49cdb5474c415050de3558b101d33
elizam21 <emuca2107@gmail.com>
2025-10-13 18:39:53 +0200

    Update zad5p.sql

--- 01_setup/zad5p.sql
+++ 01_setup/zad5p.sql
@@ -1,1 +1,7 @@
-
+DELETE FROM ksiegowosc.pracownicy
+WHERE id_pracownika IN (
+    SELECT w.id_pracownika
+    FROM ksiegowosc.wynagrodzenie w
+    JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
+    WHERE pe.kwota < 1200
+);
