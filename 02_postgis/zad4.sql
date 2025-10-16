fb9367632c1f75e7a7700f855298587765fe52b3
elizam21 <emuca2107@gmail.com>
2025-10-16 20:22:47 +0200

    Update zad4.sql

--- 02_postgis/zad4.sql
+++ 02_postgis/zad4.sql
@@ -1,1 +1,19 @@
+CREATE TABLE buildings (
+    id SERIAL PRIMARY KEY,
+    geometry GEOMETRY(Polygon), 
+    name VARCHAR(100)
+);
 
+
+CREATE TABLE roads (
+    id SERIAL PRIMARY KEY,
+    geometry GEOMETRY(LineString),
+    name VARCHAR(100)
+);
+
+
+CREATE TABLE poi (
+    id SERIAL PRIMARY KEY,
+    geometry GEOMETRY(Point),
+    name VARCHAR(100)
+);
