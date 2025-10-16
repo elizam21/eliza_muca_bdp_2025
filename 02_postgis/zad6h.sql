8dceb885c779f6f7b8ab027a7e61fd690dd9454b
elizam21 <emuca2107@gmail.com>
2025-10-16 21:02:08 +0200

    Update zad6h.sql

--- 02_postgis/zad6h.sql
+++ 02_postgis/zad6h.sql
@@ -1,1 +1,9 @@
+SELECT ST_Area(
+           ST_SymDifference(
+               bC.geometry,
+               ST_GeomFromText('POLYGON((4 7, 6 7, 6 8, 4 8, 4 7))')
+           )
+       ) AS area_diff
+FROM buildings bC
+WHERE bC.name = 'Budynek C';
 
