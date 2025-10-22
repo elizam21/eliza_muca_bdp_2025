e5f189500a256ac0a14cba30ba9d081878f6bcab
elizam21 <emuca2107@gmail.com>
2025-10-22 08:37:55 +0200

    Update zad6d.sql

--- 02_postgis/zad6d.sql
+++ 02_postgis/zad6d.sql
@@ -1,7 +1,9 @@
-SELECT 
-    name, 
-    ST_Perimeter(geometry) AS perimeter
-FROM buildings
-ORDER BY ST_Area(geometry) DESC
-LIMIT 2;
-
+SELECT name, 
+       ST_Perimeter(geometry) AS perimeter,
+       ST_Area(geometry) AS area
+FROM (
+    SELECT name, geometry,
+           DENSE_RANK() OVER (ORDER BY ST_Area(geometry) DESC) AS rank
+    FROM buildings
+) ranked
+WHERE rank <= 2;
