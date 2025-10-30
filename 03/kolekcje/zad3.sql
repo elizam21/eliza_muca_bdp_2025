7d0ed7e8e487718ff814930e1c8eed9837c13191
elizam21 <emuca2107@gmail.com>
2025-10-30 12:43:23 +0100

    Update zad3.sql

--- 03/kolekcje/zad3.sql
+++ 03/kolekcje/zad3.sql
@@ -1,3 +1,7 @@
+-- 3
+-- żeby był poligon, to obiekt musi być zamknięty, a w środku nie może być przecięć
+
+
 UPDATE obiekty
 SET geometry = ST_MakePolygon(ST_AddPoint(geometry, ST_StartPoint(geometry)))
 WHERE nazwa = 'obiekt4' AND NOT ST_IsClosed(geometry);
