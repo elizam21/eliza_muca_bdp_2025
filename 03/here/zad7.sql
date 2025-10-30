7cbe73968698526ea5c962502d9912c3295a5d9f
elizam21 <emuca2107@gmail.com>
2025-10-30 18:31:37 +0100

    Add SQL query to count sporting goods stores near parks

--- 03/here/zad7.sql
+++ 03/here/zad7.sql
@@ -1,1 +1,6 @@
-
+SELECT COUNT(*)
+FROM t2019_kar_poi_table
+JOIN land_use_a_2019
+  ON ST_DWithin(t2019_kar_poi_table.geom, land_use_a_2019.geom, 300)
+WHERE t2019_kar_poi_table.type = 'Sporting Goods Store'
+  AND land_use_a_2019.type = 'Park';
